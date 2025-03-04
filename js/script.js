28f3e697bc1f6af0d281bbc72fb0bb64c3af01e4
RaffaeleSergio <raffaele16590@gmail.com>
2025-03-04 22:29:43 +0100

    feat: condizione

--- js/script.js
+++ js/script.js
@@ -1,7 +1,53 @@
-const kmInput = document.getElementById("km")
+const trenoForm = document.getElementById("treno-form");
+const nomeInput = document.getElementById("name");
+const kmInput = document.getElementById("km");
+const etaInput = document.getElementById("eta");
+const minorenneSel = document.getElementById("eta").value = "Minorenne";
+const overSel = document.getElementById("eta").value = "Over";
+const maggiorenneSel = document.getElementById("eta").value = "Maggiorenne";
 
-const etaForm = document.getElementById("eta")
-console.log(etaForm);
+
+const prezzoStr = 0.21;
+const prezzoNum = parseInt(prezzoStr);
+const kmNum = parseInt(kmInput);
 
 
-const bottoneBtn = document.getElementById("bottone")
+
+const nomeCard = document.querySelector(".card .card-title");
+const etaCard = document.querySelector(".card .card-text");
+const prezzoCard = document.getElementById("prezzo");
+
+
+
+
+trenoForm.addEventListener("submit",gestisciForm);
+
+
+
+function gestisciForm(event) {
+    event.preventDefault();
+   let biglietto;
+if (etaInput === minorenneSel) {
+    biglietto = prezzoStr * kmInput.value - ((prezzoStr * kmInput.value) * 20) / 100
+
+} else if (etaInput === overSel) {
+    biglietto = prezzoStr * kmInput.value - ((prezzoStr * kmInput.value) * 40) / 100
+
+} else {
+     biglietto = prezzoStr * kmInput.value
+     console.log(biglietto);
+    
+}
+   
+    const nome = nomeInput.value;
+    const eta = etaInput.value;
+    const prezzo = biglietto;
+    
+
+    nomeCard.innerText = nome;
+    etaCard.innerText = eta;
+    prezzoCard.innerText = prezzo;
+
+    
+
+}
