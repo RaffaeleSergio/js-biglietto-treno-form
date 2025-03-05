6b3ad92f7a8c675f31d6fc27d897a10bc30f459c
RaffaeleSergio <raffaele16590@gmail.com>
2025-03-05 19:52:22 +0100

    feat: completato

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const trenoForm = document.getElementById("treno-form");
 const nomeInput = document.getElementById("name");
 const kmInput = document.getElementById("km");
-const etaInput = document.getElementById("eta");
-const minorenneSel = document.getElementById("eta").value = "Minorenne";
-const overSel = document.getElementById("eta").value = "Over";
-const maggiorenneSel = document.getElementById("eta").value = "Maggiorenne";
+
+const minorenneSel = "Minorenne";
+const overSel = "Over";
+const maggiorenneSel = "Maggiorenne";
 
 
 const prezzoStr = 0.21;
@@ -16,38 +16,45 @@ const kmNum = parseInt(kmInput);
 const nomeCard = document.querySelector(".card .card-title");
 const etaCard = document.querySelector(".card .card-text");
 const prezzoCard = document.getElementById("prezzo");
+const cardElem = document.querySelector(".card");
 
 
 
 
-trenoForm.addEventListener("submit",gestisciForm);
-
+trenoForm.addEventListener("submit", gestisciForm);
+let etaInput = document.getElementById("eta").value;
 
 
 function gestisciForm(event) {
     event.preventDefault();
-   let biglietto;
-if (etaInput === minorenneSel) {
-    biglietto = prezzoStr * kmInput.value - ((prezzoStr * kmInput.value) * 20) / 100
+    etaInput = document.getElementById("eta").value;
+    let biglietto;
 
-} else if (etaInput === overSel) {
-    biglietto = prezzoStr * kmInput.value - ((prezzoStr * kmInput.value) * 40) / 100
 
-} else {
-     biglietto = prezzoStr * kmInput.value
-     console.log(biglietto);
-    
-}
-   
+    if (etaInput === minorenneSel) {
+        biglietto = prezzoStr * kmInput.value - ((prezzoStr * kmInput.value) * 20) / 100
+
+
+    } else if (etaInput === overSel) {
+        biglietto = prezzoStr * kmInput.value - ((prezzoStr * kmInput.value) * 40) / 100
+
+    } else {
+        biglietto = prezzoStr * kmInput.value
+
+
+    }
+
     const nome = nomeInput.value;
-    const eta = etaInput.value;
-    const prezzo = biglietto;
-    
+    const eta = etaInput;
+    const prezzo = biglietto.toFixed(2);
+
 
     nomeCard.innerText = nome;
     etaCard.innerText = eta;
-    prezzoCard.innerText = prezzo;
+    prezzoCard.innerText = prezzo + " €";
+    cardElem.classList.remove("d-none")
 
-    
+
+
 
 }
